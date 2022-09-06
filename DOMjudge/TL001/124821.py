a9ba12b77547328a1bb457ffe59d1ca1b386cbf1
lucashsu95 <lucashsu95@gmail.com>
2022-09-06 23:38:05 +0800

    Update 124821.py

--- DOMjudge/TL001/124821.py
+++ DOMjudge/TL001/124821.py
@@ -1,12 +1,13 @@
+#TL001 字串中的每個位置到指定文字的距離
 str1 = list(input())
 str2 = input()
 ans = []
 for i in range(len(str1)):
-        ary = []
+    ary = []
     for j in range(len(str1)):
         if str1[j] == str2:
             ary.append(abs(j-i))
     ans.append(min(ary))
 
 print(ans)
-#print(str1[i], str1[j])
+
