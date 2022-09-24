a6b4c5a4bca1aa46516d5e03076f3d033f631ce6
lucashsu95 <lucashsu95@gmail.com>
2022-09-24 16:35:06 +0800

    Delete 405.py

--- TQC資訊月/python TQC 109/4/405.py
+++
@@ -1,9 +0,0 @@
-#406 不定數迴圈-分數等級
-data = int(input())
-while data != -9999:
-    if data>=90:print('A')
-    elif data>=80:print('B')
-    elif data>=70:print('C')
-    elif data>=60:print('D')
-    else:print('E')
-    data = int(input())
