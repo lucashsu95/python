060a2b0634627774dc88f05bc52941a73c78df0f
lucashsu95 <lucashsu95@gmail.com>
2022-09-24 16:35:30 +0800

    Delete 407.py

--- TQC資訊月/python TQC 109/4/407.py
+++
@@ -1,8 +0,0 @@
-#407 不定數迴圈-閏年判斷
-year = int(input())
-while year != -9999:
-    if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
-        print(f'{year} is a leap year.')
-    else:
-        print(f'{year} is not a leap year')
-    year = int(input())
